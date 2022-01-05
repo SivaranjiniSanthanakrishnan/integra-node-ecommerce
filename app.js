1ff8fb814260e8510baaca1a47135495a6ffc5df
SivaranjiniSanthanakrishnan <43466009+SivaranjiniSanthanakrishnan@users.noreply.github.com>
2022-01-05 12:02:32 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/register', registerRouter);
 app.use('/', indexRouter);
+app.use('/register', registerRouter);
 app.use(authorize.AuthorizeUser);
 app.use('/users', usersRouter);
 app.use('/product', productRouter);
